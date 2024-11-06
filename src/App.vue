3519df170290cbfc64d55dd471189eee112c267f
SimoneCerri <simone.cerri1994@gmail.com>
2024-11-06 10:00:09 +0100

    structure of the app, add bg and icon

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,14 @@ import HelloWorld from './components/HelloWorld.vue'
   <div class="container">
     <div class="row">
       <div class="col">
-        <div class="card shadow">
-          TEST
+        <div class="card shadow bg-dark text-success border-success p-3">
+          <h1 class="text-center">CURRENCY CONVERTER</h1>
+          <h4>1,00 EUR is equal to</h4>
+          <h3>1,09 USD</h3>
+          Component1
+          Component2
+          <div class="graphic">
+          </div>
         </div>
       </div>
     </div>
